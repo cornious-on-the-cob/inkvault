a0c5d11f6468c7910a2331b87777787a2022f3c4
Lorea De La Torre <loreataina@icloud.com>
2025-08-02 14:06:13 -0400

    feat: enhance Home component with navigation links, search functionality, and styling updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home'
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Logo from "../Imgs/Logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -7,19 +10,48 @@ export default function Home() {
       <nav>
         <div className="nav__links">
           <ul className="links">
-            <li className="link">Issue</li>
-            <li className="link">Character</li>
-            <li className="link">Movie</li>
+            <a href="#issues">
+              <li className="link underline bangers-regular">Issues</li>
+            </a>
+            <a href="#characters">
+              <li className="link underline bangers-regular">Characters</li>
+            </a>
+            <a href="#movies">
+              <li className="link underline bangers-regular">Movies</li>
+            </a>
           </ul>
         </div>
-        <figure className="nav__img--wrapper">
-          <img src={Logo} alt="logo" className="nav__logo" />
-        </figure>
+        <Link to="/">
+          <figure className="nav__img--wrapper">
+            <img src={Logo} alt="logo" className="nav__logo" />
+          </figure>
+        </Link>
         <div className="search__box">
           <input id="search" name="search" type="text" />
-          <button className="search__button">Search</button>
+          <button className="search__button">
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
         </div>
       </nav>
+
+      <main>
+        <section className="landing">
+          <div className="container">
+            <div className="row">
+              <h1 className="section__title bangers-regular">
+                The Vault of All Things Comic
+              </h1>
+              
+            </div>
+          </div>
+        </section>
+
+        <section className="issues">
+          <div className="container">
+            <div className="row"></div>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
